4188689fcac443d6a961092953dff1ad1da0ca05
P3trws <peu.bertoldo@gmail.com>
2024-10-30 07:26:29 -0300

    commit rapido

---
+++ Sql de inserção de coisas do banco.sql
@@ -0,0 +1,14 @@
+-- SQLite
+INSERT INTO Site_cargo(nome) VALUES 
+    ('Analista'),
+    ('Gerente'),
+    ('Coordenador'),
+    ('Auxiliar Técnico'),
+    ('Assistente Administrativo');
+
+INSERT INTO Site_skill(nome) VALUES 
+    ('Comunicação'),
+    ('Trabalho em Equipe'),
+    ('Gestão de Tempo'),
+    ('Análise de Dados'),
+    ('Resolução de Problemas');
